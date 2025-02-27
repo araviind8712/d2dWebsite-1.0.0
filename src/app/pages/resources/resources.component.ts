82abf756283ffb08b41f6acb31b22f04a644564f
Kesav-21 <cgkesav@gmail.com>
2025-02-27 12:00:48 +0530

    added Animation for text and fixed some UI

--- src/app/pages/resources/resources.component.ts
+++ src/app/pages/resources/resources.component.ts
@@ -15,18 +15,18 @@ import { trigger,state,style,transition,animate } from '@angular/animations';
       state('hidden',style({transform:'translateY(100px)',opacity:0})),
       state('visible', style({ transform: 'translateY(0)', opacity: 1 })),
       // transition('hidden => visible', animate('5000ms ease-in-out')),
-      transition('hidden => visible', animate('5.5s cubic-bezier(0.25, 1.25, 0.3, 1)'))
+      // transition('hidden => visible', animate('5.5s cubic-bezier(0.25, 1.25, 0.3, 1)'))
     ])
   ]
 })
-export class ResourcesComponent implements OnInit{
+export class ResourcesComponent{
   resources:any[]=resourceData;
-  isVisible=false;
-  ngOnInit() {
-    const card = document.getElementById('card');
-    if (card) {
-      const rect = card.getBoundingClientRect();
-      this.isVisible = rect.top < window.innerHeight - 100;
-    }
-  }
+  // isVisible=false;
+  // ngOnInit() {
+  //   const card = document.getElementById('card');
+  //   if (card) {
+  //     const rect = card.getBoundingClientRect();
+  //     this.isVisible = rect.top < window.innerHeight - 100;
+  //   }
+  // }
 }
